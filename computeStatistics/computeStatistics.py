3904cd3e58e92833cf21e07c0b943de784b89e1b
Bryam Rodriguez <brodriguezaybar@gmail.com>
2025-01-30 11:26:23 -0600

    Fix mode, add files for testing

--- computeStatistics/computeStatistics.py
+++ computeStatistics/computeStatistics.py
@@ -1,11 +1,17 @@
 # Instructions
-# Req1. The program shall be invoked from a command line. The program shall receive a file as parameter. The file will contain a list of items (presumable numbers).
-# Req 2. The program shall compute all descriptive statistics from a file containing numbers. The results shall be print on a screen and on a file named StatisticsResults.txt. All computation MUST be calculated using the basic algorithms, not functions or libraries. The descriptive statistics are mean, median, mode, standard deviation, and variance.
-# Req 3. The program shall include the mechanism to handle invalid data in the file. Errors should be displayed in the console and the execution must continue.
+# Req1. The program shall be invoked from a command line. The program shall receive a file as parameter.
+# The file will contain a list of items (presumable numbers).
+# Req 2. The program shall compute all descriptive statistics from a file containing numbers.
+# The results shall be print on a screen and on a file named StatisticsResults.txt.
+# All computation MUST be calculated using the basic algorithms, not functions or libraries.
+# The descriptive statistics are mean, median, mode, standard deviation, and variance.
+# Req 3. The program shall include the mechanism to handle invalid data in the file.
+# Errors should be displayed in the console and the execution must continue.
 # Req 4. The name of the program shall be computeStatistics.py
 # Req 5. The minimum format to invoke the program shall be as follows: python computeStatistics.py fileWithData.txt
 # Req 6. The program shall manage files having from hundreds of items to thousands of items.
-# Req 7. The program should include at the end of the execution the time elapsed for the execution and calculus of the data. This number shall be included in the results file and on the screen.
+# Req 7. The program should include at the end of the execution the time elapsed for the execution and calculus of the data.
+# This number shall be included in the results file and on the screen.
 # Req 8. Be compliant with PEP8.
 
 # Test cases and evidence
@@ -46,7 +52,7 @@ def calculate_mode(numbers):
         frequency[num] = frequency.get(num, 0) + 1
     max_freq = max(frequency.values())
     modes = [key for key, val in frequency.items() if val == max_freq]
-    return modes if len(modes) > 1 else modes[0]
+    return modes[0]
 
 def calculate_standard_deviation(variance):
     return variance ** 0.5
